156ce891513561bd6ea3b076759d461747fc6f48
=Memo <=chepkemoicynthia4@gmail.com>
2024-03-03 12:35:46 +0300

    Completed assignment

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,10 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, word_list):
+        return [w for w in word_list if sorted(w.lower()) == sorted(self.word.lower()) and w.lower() != self.word.lower()]
+    
+    def is_anagram(self, candidate):
+        return sorted(candidate.lower()) == sorted(self.word.lower())
